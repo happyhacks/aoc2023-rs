5a61a9f8b7a28a43ab4507869ca9a89379a2c3ce
happyhacks <happysingshappy@gmail.com>
2024-01-02 11:15:47 +0530

    aoc: 25: discover rustworkx

--- day25a/src/main.rs
+++ day25a/src/main.rs
@@ -2,9 +2,9 @@ use std::collections::HashMap;
 
 use petgraph::{
     dot::{Config, Dot},
-    visit::Dfs,
     Graph,
 };
+use rustworkx_core::connectivity::stoer_wagner_min_cut;
 
 fn main() {
     let h: HashMap<String, Vec<String>> = include_str!("/tmp/input.txt")
@@ -36,17 +36,23 @@ fn main() {
         println!("{:?}", Dot::with_config(&g, &[Config::EdgeNoLabel]));
     }
     // why code when dot works?
-    for cut in vec![("xxq", "hqq"), ("vkd", "qfb"), ("xzz", "kgl")] {
-        g.remove_node(*nodes.get(&cut.0.to_string()).unwrap())
-            .expect("check graph");
-        g.remove_node(*nodes.get(&cut.1.to_string()).unwrap())
-            .expect("check graph");
+    // for cut in vec![("xxq", "hqq"), ("vkd", "qfb"), ("xzz", "kgl")] {
+    //     g.remove_node(*nodes.get(&cut.0.to_string()).unwrap())
+    //         .expect("check graph");
+    //     g.remove_node(*nodes.get(&cut.1.to_string()).unwrap())
+    //         .expect("check graph");
+    // }
+    // let n = g.node_count();
+    // let mut c = 0;
+    // let mut dfs = Dfs::new(&g, g.node_indices().next().unwrap());
+    // while let Some(_) = dfs.next(&g) {
+    //     c += 1;
+    // }
+    // println!("{}", (c + 3) * (n - c + 3));
+    let min_cut_res: Result<Option<(usize, Vec<_>)>, _> =
+        stoer_wagner_min_cut(&g, |_| Ok::<usize, usize>(1));
+    if let Ok(Some((cut, part))) = min_cut_res {
+        assert!(cut == 3);
+        println!("{:?}", part.len() * (g.node_count() - part.len()));
     }
-    let n = g.node_count();
-    let mut c = 0;
-    let mut dfs = Dfs::new(&g, g.node_indices().next().unwrap());
-    while let Some(_) = dfs.next(&g) {
-        c += 1;
-    }
-    println!("{}", (c + 3) * (n - c + 3));
 }
